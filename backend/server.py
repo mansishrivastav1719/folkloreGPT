f063e27c5b53a1abb73d31b26167f8d86f516fbd
pankajsinghrsr <pankajsingh98066@gmail.com>
2026-01-09 14:55:13 +0530

    Import pipeline from transformers module

--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, Field
 from typing import List
 import uuid
 from datetime import datetime
-
+from transformers import pipeline
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
